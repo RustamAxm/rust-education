eb26c3aa3f6307165c0d521f727c7f4bf8245536
rustam <rustamaxm@gmail.com>
2025-02-14 15:57:03 +0300

    compare random

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,16 +1,33 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
 
     println!("Start game");
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).expect("failed to read");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("rand = {secret_number}");
+    
+    loop {
 
-    println!("number = {}", guess);
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("error in read");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("less"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("win");
+                break;
+            }
+
+        }
+    }
 
 }

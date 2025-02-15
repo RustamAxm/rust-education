65a4b80c101e2d1c0cd49c2191f91cf92155aa84
rustam <rustamaxm@gmail.com>
2025-02-15 19:26:54 +0300

    start annotation for lifetime

---
+++ generic/src/main.rs
@@ -0,0 +1,34 @@
+
+
+
+fn largest<T:PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    return largest;
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("result = {result}");
+    
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
+}

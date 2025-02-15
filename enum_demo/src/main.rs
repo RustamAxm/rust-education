9af85d711322d72ef37299631c4b819e85e9d7ee
rustam <rustamaxm@gmail.com>
2025-02-15 11:55:32 +0300

    enum end

--- enum_demo/src/main.rs
+++ enum_demo/src/main.rs
@@ -5,11 +5,17 @@ enum IpAddrKind {
     V6,
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -17,7 +23,18 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("quater state {state:?}");
+            25
+        }
+    }
+}
+
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
@@ -35,4 +52,23 @@ fn main() {
 
     let coin = Coin::Penny;
     println!("{}", value_in_cents(coin));
+    
+    let five = Some(5);
+    println!("function = {:?}", plus_one(five));
+    println!("function none = {:?}", plus_one(None));
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("max number {max}");
+    }
+
+    let mut count = 0;
+    let state = UsState::Alabama;
+    let coin = Coin::Quarter(state);
+    if let Coin::Quarter(state) = coin {
+        println!("state coin {state:?}");
+    } else {
+        count += 1;
+    }
+    println!("count = {count}");
 }

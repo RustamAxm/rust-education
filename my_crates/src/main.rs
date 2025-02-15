6ef2f11c4c374497776ad20cd086842f7ce16580
rustam <rustamaxm@gmail.com>
2025-02-15 15:18:49 +0300

    add mew import

--- my_crates/src/main.rs
+++ my_crates/src/main.rs
@@ -1,9 +1,13 @@
 
 mod module1;
 mod module2;
+mod new_import;
+
+use new_import::new_imp::*;
 
 fn main() {
     println!("in main");
     module1::module1::module1_print();
     module2::module2::module2_print();
+    print_from_new();
 }
---
+++ my_crates/src/new_import/new_imp.rs
@@ -0,0 +1,4 @@
+
+pub fn print_from_new() {
+    println!("from new import print");
+}

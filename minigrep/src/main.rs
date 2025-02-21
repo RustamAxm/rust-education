47c17a3f0a13a36d66837158eee100db37d83351
rustam <rustamaxm@gmail.com>
2025-02-21 15:58:03 +0300

    config parser

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,23 @@
 use std::env;
 use std::fs;
 
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    return Config { query, file_path };
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let a = &args[1];
-    let b = &args[2];
-    let file = &args[3];
-    println!("{a} {b} {file}");
-
-    let content = fs::read_to_string(file).expect("file read error");
+    let config = parse_config(&args);
+    let content = fs::read_to_string(config.file_path).expect("file read error");
     println!("content = {content}");
-    dbg!(args);
 }

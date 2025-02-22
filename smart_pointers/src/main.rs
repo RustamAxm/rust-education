f5c44ddd3aec03c7e081098a2f1224c50b61ec9d
rustam <rustamaxm@gmail.com>
2025-02-22 18:55:36 +0300

    ref counter add

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -32,6 +32,14 @@ impl Drop for CustomSmartPointer {
     }
 }
 
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
 fn main() {
     let b = Box::new(5);
     println!("b = {b}");
@@ -55,4 +63,15 @@ fn main() {
     println!("CustomSmartPointers created.");
     drop(c);
     println!("Droped c before stop main");
+
+    // ref counter
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a)); 
 }

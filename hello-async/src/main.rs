595f4742eb451249e443495aecbf6b5123eb5814
rustam <rustamaxm@gmail.com>
2025-03-01 16:20:55 +0300

    futures vector

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,5 +1,7 @@
 use trpl::Html;
 use std::time::Duration;
+use std::pin::Pin;
+use std::future::Future;
 
 
 async fn page_title(url: &str) -> Option<String> {
@@ -94,6 +96,50 @@ async fn fut_msg_multy () {
         trpl::join3(tx1_fut, tx_fut, rx_fut).await;
 }
 
+async fn multiple_futures() {
+        let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+        };
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+
+    let futures: Vec<Pin<Box<dyn Future<Output = ()>>>> =
+            vec![Box::pin(tx1_fut), Box::pin(rx_fut), Box::pin(tx_fut)];
+    trpl::join_all(futures).await;
+}
+
 
 fn main() {
     // simple spawn block
@@ -135,7 +181,7 @@ fn main() {
     }
 
 
-    {
+    /*{
         let args: Vec<String> = std::env::args().collect();
         trpl::run(async {
             let url = &args[1];
@@ -144,5 +190,9 @@ fn main() {
                 None => println!("{url} had no title"),
             }
         })
+    }*/
+
+    {
+        trpl::run( async { multiple_futures().await} );
     }
 }

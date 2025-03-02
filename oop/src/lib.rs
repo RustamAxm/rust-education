d51c8bf723af679dfbbde575498550ae9f994159
rustam <rustamaxm@gmail.com>
2025-03-02 14:40:13 +0300

    simple oop

--- oop/src/lib.rs
+++ oop/src/lib.rs
@@ -37,3 +37,48 @@ impl AveragedCollection {
         self.average = total as f64 / self.list.len() as f64;
     }
 }
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+//pub struct Screen<T: Draw> {
+//    pub components: Vec<T>,
+//}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+//impl<T> Screen<T>
+//where
+//    T: Draw,
+//{
+//    pub fn run(&self) {
+//        for component in self.components.iter() {
+//            component.draw();
+//        }
+//    }
+//}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("in draw impl");
+    }
+}
+
+
--- oop/src/main.rs
+++ oop/src/main.rs
@@ -1,7 +1,46 @@
 use oop::AveragedCollection;
+use oop::{Button, Screen};
+
+use oop::Draw;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("code to actually draw a select box");
+    }
+}
 
 fn main() {
-    let mut val = AveragedCollection::new();
-    val.add(32);
-    println!("{}", val.average());
+    {
+        let mut val = AveragedCollection::new();
+        val.add(32);
+        println!("{}", val.average());
+    }
+    {
+        let screen = Screen {
+            components: vec![
+                Box::new(SelectBox {
+                    width: 75,
+                    height: 10,
+                    options: vec![
+                        String::from("Yes"),
+                        String::from("Maybe"),
+                        String::from("No"),
+                    ],
+                }),
+                Box::new(Button {
+                    width: 50,
+                    height: 10,
+                    label: String::from("OK"),
+                }),
+            ],
+        };
+
+        screen.run();
+    }
 }
